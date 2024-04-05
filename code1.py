13ca8a5ff8f4630f565cfbfe093524c9117d2697
anamor76y <158564520+anamor76y@users.noreply.github.com>
2024-04-05 10:42:30 +0300

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,18 @@
+def filter_file(input_filename, keyword, output_filename):
+    with open(input_filename, 'r', encoding='utf-8') as input_file:
+        lines = input_file.readlines()
+    #Обираємо потрібні рядки
+    filtered_lines = [line for line in lines if keyword in line]
+
+    # Записуємо результат у новий файл
+    with open(output_filename, 'w', encoding='utf-8') as output_file:
+        output_file.writelines(filtered_lines)
+
+input_filename = "file.txt"
+keyword = "rur"
+output_filename = "filtered.txt"
+
+# Викликаємо функцію для виконання фільтрації
+filter_file(input_filename, keyword, output_filename)
+
+print(f"Результат фільтрації збережено у файлі '{output_filename}'")
